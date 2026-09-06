bdca0c26822d2dba49fe034a8e9e9411e6f488bb
Bernat Mestres <ber.mestres@gmail.com>
2026-09-06 16:30:55 +0200

    feat: ExecutiveMenu class

---
+++ builder_pattern/src/main/java/ExecutiveMenu.java
@@ -0,0 +1,12 @@
+public class ExecutiveMenu {
+
+    private String starter;
+    private boolean isVegan;
+    private boolean isGlutenFree;
+    private String mainCourse;
+    private String suplement;
+    private String desset;
+
+    private ExecutiveMenu(){
+    }
+}
